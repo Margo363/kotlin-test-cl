4c2973973558b7b4a775870b09e7ea924795e1f3
Margarita Anisimova <anisimova.post@gmail.com>
2022-01-31 18:00:52 +0300

    добавлены автотесты

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,12 +7,13 @@ fun commissionPay (
     val noCommissionVkPay = amount * 0 //комиссия 0% Vk Pay
     val limitSinglePaymentVkPay = 1_500_000 //максимальная сумма перевода единовременно в копейках Vk Pay
     val limitInMonthMaxVkPay = 4_000_000 //максимальная сумма перевода в месяц в копейках Vk Pay
+    val limitNull = null
 
     val limitInMonthOther = 60_000_000 //максимальная сумма перевода в месяц в копейках Visa/Mir
     val limitInDayOther = 15_000_000 //максимальная сумма перевода в сутки в копейках Visa/Mir/Mastercard/Maestro
 
     val commissionPercentageVisaMir = amount / 10000 * 75 //комиссия 0.75% от суммы перевода Visa/Mir
-    val commissionMinVisaMir = 3500 //комиссия минимум 35 рублей = 3500 коп Visa/Mir
+    val commissionMinVisaMir = 3500 //комиссия минимум 35 рублей = 3500 копеек Visa/Mir
 
     val limitInMonthMastercardMaestro = 7_500_000 //максимальная сумма перевода в месяц в копейках Mastercard/Maestro
     val noCommissionMastercardMaestro = amount * 0 // комиссия 0% от суммы перевода до 7_500_000 Mastercard/Maestro
@@ -25,14 +26,14 @@ fun commissionPay (
         1 -> {
             when {
                 amount<=limitSinglePaymentVkPay -> noCommissionVkPay
-                limitInMonth>limitInMonthMaxVkPay -> println("Внимание! Перевод недоступен, максимальная сумма всех переводов в месяц = 40000 рублей.")
-                else -> println("Внимание! Перевод недоступен, максимальная сумма единовременного перевода = 15000 рублей.")
+                limitInMonth>limitInMonthMaxVkPay -> limitNull//println("Внимание! Перевод недоступен, максимальная сумма всех переводов в месяц = 40000 рублей.")
+                else -> limitNull//println("Внимание! Перевод недоступен, максимальная сумма единовременного перевода = 15000 рублей.")
             }
         }
         2 -> {
             when {
-                limitInMonth>limitInMonthOther -> println("Внимание! Перевод недоступен, максимальная сумма всех переводов в месяц = 600000 рублей.")
-                amount>limitInDayOther -> println("Внимание! Перевод не доступен, максимальная сумма переводов в сутки составляет 150000 рублей")
+                limitInMonth>limitInMonthOther -> limitNull //println("Внимание! Перевод недоступен, максимальная сумма всех переводов в месяц = 600000 рублей.")
+                amount>limitInDayOther -> limitNull//println("Внимание! Перевод не доступен, максимальная сумма переводов в сутки составляет 150000 рублей")
                 commissionPercentageVisaMir>commissionMinVisaMir -> commissionPercentageVisaMir
                 else -> commissionMinVisaMir
             }
@@ -47,15 +48,15 @@ fun commissionPay (
     return cardType
 }
 
-fun main() {
-    val cardType: Int = 1 //1="VkPay",2="VisaMir",3="MastercardMaestro" //тип платежной системы
-    val limitInMonth: Int = 8_000_000
-    val amount: Int = 8_320_000 //сумма перевода в копейках
-
-
-    val result: Int = commissionPay(cardType, limitInMonth, amount)
-
-}
+//fun main() {
+//    val cardType: Int = 1 //1="VkPay",2="VisaMir",3="MastercardMaestro" //тип платежной системы
+//    val limitInMonth: Int = 8_000_000
+//    val amount: Int = 8_320_000 //сумма перевода в копейках
+//
+//
+//    val result: Int = commissionPay(cardType, limitInMonth, amount)
+//
+//}
 
 
 
--- src/test/kotlin/ru/netology/MainKtTest.kt
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -8,29 +8,76 @@ import org.junit.Assert.*
 class MainKtTest {
 
     @Test
-    fun commissionPay_test_VkPay_noCommissionVkPay() {
-        //arrange
-        val testVkPayCardType = 1 //1="VkPay"//тип платежной системы
-        val testVkPayLimitInMonth = 0
-        val testVkPayAmount = 100
-
-        val noCommissionTestVkPay = testVkPayAmount * 0 //комиссия 0% Vk Pay
-        val limitSinglePaymentVkPay = 1_500_000 //максимальная сумма перевода единовременно в копейках Vk Pay
-        val limitInMonthMaxVkPay = 4_000_000 //максимальная сумма перевода в месяц в копейках Vk Pay
-
-        //act
-        val result = noCommissionTestVkPay
-
-        //assert
-        assertEquals(0, result)
-    }
-
-    @Test
     fun commissionPay_test_VkPay() {
         //arrange
         val testVkPayCardType = 1 //1="VkPay"//тип платежной системы
         val testVkPayLimitInMonth = 0
         val testVkPayAmount = 1
+
+        val noCommissionVkPay = testVkPayAmount * 0 //комиссия 0% Vk Pay
+        val limitSinglePaymentVkPay = 1_500_000 //максимальная сумма перевода единовременно в копейках Vk Pay
+        val limitInMonthMaxVkPay = 4_000_000 //максимальная сумма перевода в месяц в копейках Vk Pay
+
+        //act
+        val result = commissionPay(
+            cardType = testVkPayCardType,
+            limitInMonth = testVkPayLimitInMonth,
+            amount = testVkPayAmount
+        )
+
+        //assert
+        assertEquals(1, result)
+    }
+    @Test
+    fun commissionPay_test_limitInMonthMaxVkPay() {
+        //arrange
+        val testVkPayCardType = 1 //1="VkPay"//тип платежной системы
+        val testVkPayLimitInMonth = 8_600_000
+        val testVkPayAmount = 1
+
+        val noCommissionVkPay = testVkPayAmount * 0 //комиссия 0% Vk Pay
+        val limitSinglePaymentVkPay = 1_500_000 //максимальная сумма перевода единовременно в копейках Vk Pay
+        val limitInMonthMaxVkPay = 4_000_000 //максимальная сумма перевода в месяц в копейках Vk Pay
+
+        //act
+        val result = commissionPay(
+            cardType = testVkPayCardType,
+            limitInMonth = testVkPayLimitInMonth,
+            amount = testVkPayAmount
+        )
+
+        //assert
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun commissionPay_test_limitInMonthMaxVkPay__() {
+        //arrange
+        val testVkPayCardType = 1 //1="VkPay"//тип платежной системы
+        val testVkPayLimitInMonth = 1
+        val testVkPayAmount = 6_000_000
+
+        val noCommissionVkPay = testVkPayAmount * 0 //комиссия 0% Vk Pay
+        val limitSinglePaymentVkPay = 1_500_000 //максимальная сумма перевода единовременно в копейках Vk Pay
+        val limitInMonthMaxVkPay = 4_000_000 //максимальная сумма перевода в месяц в копейках Vk Pay
+
+        //act
+        val result = commissionPay(
+            cardType = testVkPayCardType,
+            limitInMonth = testVkPayLimitInMonth,
+            amount = testVkPayAmount
+        )
+
+        //assert
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun commissionPay_test_limitInMonthMaxVkPay_() {
+        //arrange
+        val testVkPayCardType = 1 //1="VkPay"//тип платежной системы
+        val testVkPayLimitInMonth = 6_000_000
+        val testVkPayAmount = 6_000_000
 
         val noCommissionVkPay = testVkPayAmount * 0 //комиссия 0% Vk Pay
         val limitSinglePaymentVkPay = 1_500_000 //максимальная сумма перевода единовременно в копейках Vk Pay
@@ -55,18 +102,97 @@ class MainKtTest {
         val testVisaMirLimitInMonth = 50_000_000
         val testVisaMirAmount = 10_000_000
 
-        val limitInMonthOther = 60_000_000 //максимальная сумма перевода в месяц в копейках Visa/Mir
-        val limitInDayOther = 15_000_000 //максимальная сумма перевода в сутки в копейках Visa/Mir/Mastercard/Maestro
+        val testLimitInMonthOther = 60_000_000 //максимальная сумма перевода в месяц в копейках Visa/Mir
+        val testLimitInDayOther = 15_000_000 //максимальная сумма перевода в сутки в копейках Visa/Mir/Mastercard/Maestro
 
-        val commissionPercentageVisaMir = testVisaMirAmount / 10000 * 75 //комиссия 0.75% от суммы перевода Visa/Mir
-        val commissionMinVisaMir = 3500 //комиссия минимум 35 рублей = 3500 коп Visa/Mir
+        val testCommissionPercentageVisaMir = testVisaMirAmount / 10000 * 75 //комиссия 0.75% от суммы перевода Visa/Mir
+        val testCommissionMinVisaMir = 3500 //комиссия минимум 35 рублей = 3500 коп Visa/Mir
 
         //act
         val result = commissionPay(
             cardType = testVisaMirType,
             limitInMonth = testVisaMirLimitInMonth,
-            amount = testVisaMirAmount
+            amount = testVisaMirAmount,
+
         )
+
+        //assert
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun commissionPay_test_VisaMir_() {
+
+        //arrange
+        val testVisaMirType = 2 //2="VisaMir" //тип платежной системы
+        val testVisaMirLimitInMonth = 0
+        val testVisaMirAmount = 10
+
+        val testLimitInMonthOther = 60_000_000 //максимальная сумма перевода в месяц в копейках Visa/Mir
+        val testLimitInDayOther = 15_000_000 //максимальная сумма перевода в сутки в копейках Visa/Mir/Mastercard/Maestro
+
+        val testCommissionPercentageVisaMir = testVisaMirAmount / 10000 * 75 //комиссия 0.75% от суммы перевода Visa/Mir
+        val testCommissionMinVisaMir = 3500 //комиссия минимум 35 рублей = 3500 коп Visa/Mir
+
+        //act
+        val result = commissionPay(
+            cardType = testVisaMirType,
+            limitInMonth = testVisaMirLimitInMonth,
+            amount = testVisaMirAmount,
+
+            )
+
+        //assert
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun commissionPay_test_VisaMir__() {
+
+        //arrange
+        val testVisaMirType = 2 //2="VisaMir" //тип платежной системы
+        val testVisaMirLimitInMonth = 80_000_000
+        val testVisaMirAmount = 10
+
+        val testLimitInMonthOther = 60_000_000 //максимальная сумма перевода в месяц в копейках Visa/Mir
+        val testLimitInDayOther = 15_000_000 //максимальная сумма перевода в сутки в копейках Visa/Mir/Mastercard/Maestro
+
+        val testCommissionPercentageVisaMir = testVisaMirAmount / 10000 * 75 //комиссия 0.75% от суммы перевода Visa/Mir
+        val testCommissionMinVisaMir = 3500 //комиссия минимум 35 рублей = 3500 коп Visa/Mir
+
+        //act
+        val result = commissionPay(
+            cardType = testVisaMirType,
+            limitInMonth = testVisaMirLimitInMonth,
+            amount = testVisaMirAmount,
+
+            )
+
+        //assert
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun commissionPay_test_VisaMir___() {
+
+        //arrange
+        val testVisaMirType = 2 //2="VisaMir" //тип платежной системы
+        val testVisaMirLimitInMonth = 0
+        val testVisaMirAmount = 1_000_000_000
+
+        val testLimitInMonthOther = 60_000_000 //максимальная сумма перевода в месяц в копейках Visa/Mir
+        val testLimitInDayOther = 15_000_000 //максимальная сумма перевода в сутки в копейках Visa/Mir/Mastercard/Maestro
+
+        val testCommissionPercentageVisaMir = testVisaMirAmount / 10000 * 75 //комиссия 0.75% от суммы перевода Visa/Mir
+        val testCommissionMinVisaMir = 3500 //комиссия минимум 35 рублей = 3500 коп Visa/Mir
+
+        //act
+        val result = commissionPay(
+            cardType = testVisaMirType,
+            limitInMonth = testVisaMirLimitInMonth,
+            amount = testVisaMirAmount,
+
+            )
 
         //assert
         assertEquals(2, result)
@@ -98,21 +224,59 @@ class MainKtTest {
     }
 
     @Test
-    fun main_test() {
+    fun commissionPay_test_MastercardMaestro_() {
+
         //arrange
-        val testMainAmount = 8_320_000 //сумма перевода в копейках
-        val testMainLimitInMonth = 8000000
-        val testMainCardType = 2 //1="VkPay",2="VisaMir",3="MastercardMaestro" //тип платежной системы
+        val testMastercardCardType = 3 //3="MastercardMaestro" //тип платежной системы
+        val testMastercardLimitInMonth = 9_000_000
+        val testMastercardAmount = 10
+
+        val limitInMonthMastercardMaestro =
+            7_500_000 //максимальная сумма перевода в месяц в копейках Mastercard/Maestro
+        val noCommissionMastercardMaestro =
+            testMastercardAmount * 0 // комиссия 0% от суммы перевода до 7_500_000 Mastercard/Maestro
+        val commissionPercentageMastercardMaestro =
+            testMastercardAmount / 1_000 * 6 //комиссия 0.6% от суммы перевода Mastercard/Maestro
+        val commissionMinMastercardMaestro = 2_000 // минимальная комиссия 20 рублей = 2000 коп  Mastercard/Maestro
+        val commissionMastercardMaestro =
+            commissionPercentageMastercardMaestro + commissionMinMastercardMaestro //комиссия 0.6% + 20 рублей Mastercard/Maestro
 
         //act
         val result = commissionPay(
-            amount = testMainAmount,
-            limitInMonth = testMainLimitInMonth,
-            cardType = testMainCardType
+            cardType = testMastercardCardType,
+            limitInMonth = testMastercardLimitInMonth,
+            amount = testMastercardAmount
         )
 
         //assert
-        assertEquals(2, result)
+        assertEquals(3, result)
     }
+    @Test
+    fun commissionPay_test_MastercardMaestro_2() {
 
+        //arrange
+        val testMastercardCardType = 3 //3="MastercardMaestro" //тип платежной системы
+        val testMastercardLimitInMonth = 0
+        val testMastercardAmount = 1000
+
+        val limitInMonthMastercardMaestro =
+            7_500_000 //максимальная сумма перевода в месяц в копейках Mastercard/Maestro
+        val noCommissionMastercardMaestro =
+            testMastercardAmount * 0 // комиссия 0% от суммы перевода до 7_500_000 Mastercard/Maestro
+        val commissionPercentageMastercardMaestro =
+            testMastercardAmount / 1_000 * 6 //комиссия 0.6% от суммы перевода Mastercard/Maestro
+        val commissionMinMastercardMaestro = 2_000 // минимальная комиссия 20 рублей = 2000 коп  Mastercard/Maestro
+        val commissionMastercardMaestro =
+            commissionPercentageMastercardMaestro + commissionMinMastercardMaestro //комиссия 0.6% + 20 рублей Mastercard/Maestro
+
+        //act
+        val result = commissionPay(
+            cardType = testMastercardCardType,
+            limitInMonth = testMastercardLimitInMonth,
+            amount = testMastercardAmount
+        )
+
+        //assert
+        assertEquals(3, result)
+    }
 }
